f8450b61a7cdce3575345aafc4dfcd1072ff7608
Luiz Fernando <lfernando-silva@hotmail.com>
2017-09-14 15:58:15 -0300

    Update Notify.js

--- src/actions/Notify.js
+++ src/actions/Notify.js
@@ -1,3 +1,4 @@
+import Promise from 'bluebird'
 import * as Firebase from 'firebase'
 import axios from 'axios'
 
@@ -7,52 +8,40 @@ Firebase.initializeApp(config)
 const Notifier = Firebase.messaging()
 
 Notifier.onMessage(message => {
-    //Fazer alguma coisa com a mensagem
-    console.log(message)
+    console.log(message) //Fazer alguma coisa com a mensagem
 })
 
-export const notifyFCM = () => {
+const getToken = () => {
     return Notifier
         .requestPermission()
         .then(() => Notifier.getToken())
-        .then(token => {
-            return axios({
-                method: 'post',
-                url: `https://iid.googleapis.com/iid/v1/${token}/rel/topics/inerge_info`,
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `key=${config.apiKey}`
-                }
-            }).then(response => {
-                console.log(response)
-            }).catch(err => {
-                console.log(err)
-            })
-        })
         .catch(err => console.log("Sem permissão " + err))
 }
 
-export const unsubscribe = () => {
-    return Notifier
-        .requestPermission()
-        .then(() => Notifier.getToken())
-        .then(token => {
-            return axios({
-                method: 'post',
-                url: `https://iid.googleapis.com/iid/v1:batchRemove`,
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `key=${config.apiKey}`
-                },
-		data: {
-   			"to": "/topics/inerge_info",
-   			"registration_tokens": [token],
-		}
-            }).then(response => {
-                console.log(response)
-            }).catch(err => {
-                console.log(err)
-            })
-        })
-        .catch(err => console.log("Sem permissão " + err))
+const makeRequest = (path,data) => {
+    let request = Object.assign({
+        method: 'post',
+        url: `https://iid.googleapis.com/iid/v1${path}`,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `key=${config.apiKey}`
+        }
+    }, data)
+    return axios(request)
+}
+
+const makeTopicRequest = (token,topic) => makeRequest(`/${token}/rel/topics/${topic}`)
+
+const makeUnsubscribeRequest = (tokens, topic) => makeRequest(`:batchRemove`,{
+    "to": topic,
+    "registration_tokens": tokens, //ARRAY DE TOKENS
+})
+
+export const unsubscribe = topic => getToken().then(token => makeUnsubscribeRequest([token], topic))
+
+export const listenFCM = params => {
+    return Promise.all([
+        listenWeather(),
+        listenDevices(`user_id_${params.userId}`)
+    ])
 }
